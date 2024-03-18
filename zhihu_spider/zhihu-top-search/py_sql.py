4b7e94c4cd8eb928973a8c5ba69b7c091aed92bb
unknown <fuhongrui@lettucegames.com>
2024-03-18 19:03:46 +0800

    zhihu

--- zhihu_spider/zhihu-top-search/py_sql.py
+++ zhihu_spider/zhihu-top-search/py_sql.py
@@ -1,40 +1,106 @@
+
 import pymysql
 
 """
 
 CREATE TABLE `hot_search` (
   `id` INT AUTO_INCREMENT PRIMARY KEY COMMENT '热搜记录 ID',
-  `article_id` INT AUTO_INCREMENT PRIMARY KEY COMMENT '文章 ID',
-  `author_id` INT  COMMENT '作者 ID',
-  `author_name` VARCHAR(255)  COMMENT '作者姓名',
-  `article_title` text   COMMENT '文章标题',
-  `follow_count` INT  COMMENT '关注数量',
-  `answer_count` INT  COMMENT '回答数',
-  `like_count` INT  COMMENT '点赞数',
-  `view_count` INT  COMMENT '浏览量',
+  `article_id` INT NOT NULL COMMENT '文章 ID',
+  `article_title` TEXT NOT NULL COMMENT '文章 标题',
+  `create_time` int  COMMENT '创建时间',
+  `hot_search_time` INT   COMMENT '上热搜时间',
   `duration` INT  COMMENT '持续时间',
-  `tag_category` text  COMMENT '标签分类（多个）', # 为一个list关联tags表
-  `heat` INT  COMMENT '热度',
-  `status` TINYINT  DEFAULT 0 COMMENT '状态',
-  `date` DATE  COMMENT '日期'
+  `tag_category` TEXT  COMMENT '标签（列表）对应tags表的id，用,分割',
+  `heat_max` BIGINT COMMENT '最高热度',
+  `heat_min` BIGINT COMMENT '最低热度',
+  `heat_avg` BIGINT COMMENT '平均热度=heat_sum/version',
+  `heat_sum` BIGINT COMMENT '总计热度',
+  `answer_count` BIGINT  COMMENT '回答数量',
+  `follow_count` BIGINT COMMENT '关注数',
+  `view_count` BIGINT  COMMENT '浏览量',
+  `dates` VARCHAR(100) NOT NULL COMMENT '日期',
+  `version` INT COMMENT '爬取次数'
 );
 
 CREATE TABLE `tags` (
-  `id` INT AUTO_INCREMENT PRIMARY KEY COMMENT '热搜记录 ID',
+  `id` INT AUTO_INCREMENT PRIMARY KEY COMMENT '标签 ID',
   `tag_name` VARCHAR(255)  COMMENT '标签名称',
-  `quantity` int,
+  `quantity` int
 );
 """
 
-
 import pymysql
 
+host='localhost'
+port=3306
+user='root'
+password='123456'
+database='jd_spider'
+
+
 class HotSearch:
-    def __init__(self, host, username, password, database, port):
+    def __init__(self):
         """
         初始化数据库连接
         """
-        self.db = pymysql.connect(host, username, password, database, port, charset='utf8')
+        self.conn = pymysql.connect(host=host, port=port, user=user, password=password, database=database)
+        self.cursor = self.conn.cursor()
+
+    def __del__(self):
+        """
+        关闭数据库连接
+        """
+        self.cursor.close()
+        self.conn.close()
+
+    def insert_batch(self, data_list):
+        """
+        批量插入数据
+        """
+        sql = """
+        INSERT INTO hot_search (article_id, article_title, create_time, hot_search_time, duration, tag_category, heat_max, heat_min, heat_avg, heat_sum, answer_count, follow_count, view_count, dates, version)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        self.cursor.executemany(sql, data_list)
+        self.conn.commit()
+
+    def delete(self, article_id):
+        """
+        根据文章 ID 删除数据
+        """
+        sql = """
+        DELETE FROM hot_search WHERE article_id = %s
+        """
+        self.cursor.execute(sql, (article_id,))
+        self.conn.commit()
+
+    def update(self, article_id, duration, heat_max, heat_min, heat_avg, heat_sum, answer_count, follow_count, view_count, datetime_str, version):
+        """
+        根据文章 ID 修改数据
+        """
+        sql = """
+        UPDATE hot_search SET duration = %s, heat_max = %s, heat_min = %s, heat_avg = %s, heat_sum = %s, answer_count = %s, follow_count = %s, view_count = %s, version = %s, dates = %s WHERE article_id = %s
+        """
+        self.cursor.execute(sql, (duration, heat_max, heat_min, heat_avg, heat_sum, answer_count, follow_count, view_count, version, datetime_str, article_id))
+        self.conn.commit()
+
+    def query_by_article_id(self, article_id):
+        """
+        根据文章 ID 查询数据
+        """
+        sql = """
+        SELECT * FROM hot_search WHERE article_id = %s
+        """
+        self.cursor.execute(sql, (article_id,))
+        return self.cursor.fetchone()
+
+
+class Tags:
+    def __init__(self):
+        """
+        初始化数据库连接
+        """
+        self.db = pymysql.connect(host=host, port=port, user=user, password=password, database=database, charset='utf8')
         self.cursor = self.db.cursor()
 
     def __del__(self):
@@ -44,54 +110,53 @@ class HotSearch:
         self.cursor.close()
         self.db.close()
 
-    def insert(self, author_id, author_name, article_title, follow_count, answer_count, like_count, view_count, duration, tag_category, heat, date):
+    def insert(self, tag_name, quantity):
         """
         插入数据
         """
         sql = """
-        INSERT INTO hot_search (author_id, author_name, article_title, follow_count, answer_count, like_count, view_count, duration, tag_category, heat, date)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        INSERT INTO tags (tag_name, quantity)
+        VALUES (%s, %s)
         """
-        self.cursor.execute(sql, (author_id, author_name, article_title, follow_count, answer_count, like_count, view_count, duration, tag_category, heat, date))
+        self.cursor.execute(sql, (tag_name, quantity))
         self.db.commit()
 
-    def delete(self, id):
+    def delete(self, tag_id):
         """
-        删除数据
+        根据标签 ID 删除数据
         """
         sql = """
-        DELETE FROM hot_search WHERE id = %s
+        DELETE FROM tags WHERE id = %s
         """
-        self.cursor.execute(sql, (id,))
+        self.cursor.execute(sql, (tag_id,))
         self.db.commit()
 
-    def update(self, article_id, author_id, author_name, article_title, follow_count, answer_count, like_count, view_count, duration, tag_category, heat, date):
+    def update(self, tag_id, quantity):
         """
-        更新数据
+        根据标签 ID 修改数据
         """
         sql = """
-        UPDATE hot_search SET author_id = %s, author_name = %s, article_title = %s, follow_count = %s, answer_count = %s, like_count = %s, view_count = %s, duration = %s, tag_category = %s, heat = %s, date = %s WHERE article_id = %s
+        UPDATE tags SET quantity = %s WHERE id = %s
         """
-        self.cursor.execute(sql, (author_id, author_name, article_title, follow_count, answer_count, like_count, view_count, duration, tag_category, heat, date, article_id))
+        self.cursor.execute(sql, (quantity, tag_id))
         self.db.commit()
-
-    def query_one(self, article_id):
-        """
-        查询单条数据
-        """
-        sql = """
-        SELECT * FROM hot_search WHERE article_id = %s
-        """
-        self.cursor.execute(sql, (article_id,))
-        return self.cursor.fetchone()
 
     def query_all(self):
         """
         查询所有数据
         """
         sql = """
-        SELECT * FROM hot_search
+        SELECT * FROM tags
         """
         self.cursor.execute(sql)
         return self.cursor.fetchall()
 
+    def query_by_id(self, tag_name):
+        """
+        根据标签 ID 查询数据
+        """
+        sql = """
+        SELECT * FROM tags WHERE tag_name = %s
+        """
+        self.cursor.execute(sql, (tag_name,))
+        return self.cursor.fetchone()
--- zhihu_spider/zhihu-top-search/zhihu.py
+++ zhihu_spider/zhihu-top-search/zhihu.py
@@ -1,57 +1,123 @@
 # -*- coding=utf-8 -*-
 
-from datetime import datetime
-import json
-import os
-import re
-
-from lxml import etree
+import time
+import random
 import requests
 
-import log_tools
+from lxml import etree
+from bs4 import BeautifulSoup
+from py_sql import HotSearch,Tags
+
 
 
 BASE_URL = 'https://www.zhihu.com'
-JSON_DIR = './raw'
-ARCHIVE_DIR = './archives'
-LOG_DIR = './logs'
-
-logger = log_tools.init_logger(__name__, log_path=LOG_DIR)
 
 
-def get_top(url):
-    ''' 获取网页 HTML 返回字符串
+class Spider:
+    def __init__(self):
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
+        }
 
-    Args:
-        url: str, 网页网址
-    Returns:
-        HTML 字符串
-    '''
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
-    }
-    response = requests.get(url, headers=headers)
-    return response.json()
+    def get_top(self,url):
+        ''' 获取网页 HTML 返回字符串
 
+        Args:
+            url: str, 网页网址
+        Returns:
+            HTML 字符串
+        '''
 
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    def details_page(self,url):
+        response = requests.get(url, headers=self.headers)
+
+        soup = BeautifulSoup(response.content.decode("utf8"), 'lxml')
+        dic_lxml = soup.find(name="div",attrs={"class":"QuestionHeader-topics"})
+        label_list = dic_lxml.find_all(name="div",attrs={"class":"css-1gomreu"})
+        tag_category = []
+        for i in label_list:
+            tag_category.append(i.text)
+        num_lxml = soup.find_all(name="div",attrs={"class":"NumberBoard-itemInner"})
+        follow_count, view_count = num_lxml
+        follow_count = follow_count.find(name="strong").text
+        follow_count = follow_count.replace(",", "")
+        view_count = view_count.find(name="strong").text
+        view_count = view_count.replace(",", "")
+        print("    [INFO] Get hot search details [{}-{}]".format( follow_count, view_count))
+        return tag_category, follow_count, view_count
+
+    def save(self, article_id, article_title, create_time, hot_search_time, tag_category, heat, answer, follow_count, view_count):
+        # 获取当前时间戳
+        timestamp = time.time()
+        # 将时间戳转换为日期时间
+        datetime_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
+        sql = HotSearch()
+        sql_data = sql.query_by_article_id(article_id)
+        # 之前不存在热榜的话题
+        if not sql_data:
+            sql_tag = Tags()
+            tag_id_list = []
+            print("    [INFO] Save label data [{}]".format(tag_category))
+            for tag_name in tag_category:
+                tag_data = sql_tag.query_by_id(tag_name)
+                if tag_data:
+                    tag_id = tag_data[0]
+                    quantity = tag_data[2]+1
+                    sql_tag.update(tag_id, quantity)
+                else:
+                    sql_tag.insert(tag_name, 1)
+                    tag_data = sql_tag.query_by_id(tag_name)
+                tag_id_list.append(tag_data[0])
+            print("    [INFO] Save article data [{}]".format(article_title))
+            tag_id_str = ",".join([str(i) for i in tag_id_list])
+            data_list = [(article_id, article_title, create_time, hot_search_time, 1, tag_id_str, heat, heat, heat, heat, answer, follow_count, view_count, datetime_str, 1)]
+            sql.insert_batch(data_list)
+        #已经存在热榜的话题，还未下去
+        else:
+            print("    [INFO] Save article data [{}]".format(article_title))
+            heat_max = sql_data[7]
+            heat_min = sql_data[8]
+            heat_sum = sql_data[9]
+            if heat > heat_max:
+                heat_max = heat
+            elif heat < heat_min:
+                heat_min = heat
+            heat_sum += heat
+            version = sql_data[-1]
+            version += 1
+            heat_avg = heat_sum/version
+            duration = sql_data[5] + 1
+            sql.update(article_id, duration, heat_max, heat_min, heat_avg, heat_sum, answer, follow_count, view_count, datetime_str, version)
 
 
 def main():
     try:
         top_url = "/api/v3/feed/topstory/hot-lists/total?limit=100"
-        data_json = get_top(BASE_URL + top_url)
-        print(data_json)
-        print(type(data_json))
-        depth_url = BASE_URL + "/question/{}?utm_division=hot_list_page"
-        for data in data_json.get("data"):
+        print(" crawl Hot search list ...")
+
+        spider = Spider()
+        data_json = spider.get_top(BASE_URL + top_url)
+        new = int(time.time())
+        for i,data in enumerate(data_json.get("data")):
             target = data.get("target")
+            print("[INFO {}/{}] Get hot search information [{}]".format(i, len(data_json.get("data")), target.get("title")))
             id = target["id"]
-            print(id)
-            print(target)
-            print(type(target))
+            depth_url = BASE_URL + "/question/{}?utm_division=hot_list_page"
+            depth_url = depth_url.format(id)
+            tag_category, follow_count, view_count = spider.details_page(depth_url)
+            article_title = target.get("title")
+            create_time = target.get("created")
+            heat = data.get("detail_text")
+            heat = int(heat.replace("热度","").replace(" ","").replace("万","")) * 10000
+            answer = target.get("answer_count")
+            spider.save(id,article_title, create_time, new, tag_category, heat, answer, follow_count, view_count)
+            time.sleep(random.randint(8,20))
+
 
     except Exception as e:
-        logger.exception(e)
         raise e
 
 
